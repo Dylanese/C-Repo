43e18d9f088998a3646e5dca28de00d48f4a95f2
Dylan <847087675@qq.com>
2022-07-12 23:11:55 +0800

    Adapter

--- StructurePattern.cpp
+++ StructurePattern.cpp
@@ -294,7 +294,57 @@ void Process(){
 
 
 
+/*********** 适配器模式 ***********/
 
+//遗留接口（老接口）
+class IAdaptee{
+public:
+    virtual void foo(int data)=0;
+    virtual int bar()=0;
+};
+//遗留类型
+class OldClass: public IAdaptee{
+    //....
+};
+
+
+//目标接口（新接口）
+class ITarget{
+public:
+    virtual void process()=0;
+};
+//对象适配器
+class Adapter: public ITarget{  /* 继承 */
+protected:
+    IAdaptee* pAdaptee;         /* 组合 */
+public:
+    Adapter(IAdaptee* pAt) : pAdaptee(pAt) { }
+    virtual void process(){
+        int data=pAdaptee->bar();
+        pAdaptee->foo(data);
+    }
+};
+
+//类适配器
+class Adapter: public ITarget, protected OldClass{ //多继承      
+}
+
+int main(){
+    IAdaptee* pAdaptee=new OldClass();
+    ITarget* pTarget=new Adapter(pAdaptee);
+    pTarget->process();
+    return 0;
+}
+
+template<typename T>
+class stack{
+    deque<T> container;
+    
+};
+template<typename T>
+class queue{
+    deque<T> container;
+};
 
 
 
